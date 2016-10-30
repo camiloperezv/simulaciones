b9df032f69ba113ca0ba0ccb67d02a688ada7015
Camilo Pérez <cperezv@insite.com.co>
2016-10-30 17:08:16 -0500

    código de svm, bug en linea 290

--- cod_matlab/Main.m
+++ cod_matlab/Main.m
@@ -219,4 +219,87 @@ elseif punto==3
     disp(Texto);
 
     %%% Fin punto de clasificaci?n %%%
+elseif punto == 4
+    for gamma=[0.01 0.1 1 10 100];
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%                                      %%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%         LAB 5                        %%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%         SVM CLASSIFICATION           %%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%                                      %%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%% punto clasificaci???n %%%
+
+        NumClases=length(unique(Y1)); %%% Se determina el n???mero de clases del problema.
+        EficienciaTest=zeros(1,Rept);
+        NumMuestras=size(X1,1);
+        rng('default');
+        boxConstraint=100;
+        particion=cvpartition(NumMuestras,'Kfold',Rept);
+        for fold=1:Rept
+
+            %%% Se hace la partici???n de las muestras %%%
+            %%%      de entrenamiento y prueba       %%%
+
+
+            Xtrain=X1(particion.training(fold),:);
+            Xtest=X1(particion.test(fold),:);
+            Ytrain=Y1(particion.training(fold),:);
+            Ytest=Y1(particion.test(fold));
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            %%% Se normalizan los datos %%%
+
+            [Xtrain,mu,sigma]=zscore(Xtrain);
+            Xtest=(Xtest - repmat(mu,size(Xtest,1),1))./repmat(sigma,size(Xtest,1),1);
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            %%% Entrenamiento de los modelos. Se usa la metodologia One vs All. %%%
+
+            %%% Complete el codigo implimentando la estrategia One vs All.
+            %%% Recuerde que debe de entrenar un modelo SVM para cada clase.
+            %%% Solo debe de evaluar las muestras con conflicto.
+
+            Ytrain1 = Ytrain;
+            Ytrain1(Ytrain ~= 1) = -1;
+            %Modelo1=entrenarSVM(Xtrain,Ytrain1,'classification',boxConstraint,gamma,'lin_kernel');
+            Modelo1=entrenarSVM(Xtrain,Ytrain1,'classification',boxConstraint,gamma);
+            Ytrain2 = Ytrain;
+            Ytrain2(Ytrain ~= 2) = -1;
+            Ytrain2(Ytrain == 2) = 1;
+            %Modelo2=entrenarSVM(Xtrain,Ytrain2,'classification',boxConstraint,gamma,'lin_kernel');
+            Modelo2=entrenarSVM(Xtrain,Ytrain2,'classification',boxConstraint,gamma);
+            Ytrain3 = Ytrain;
+            Ytrain3(Ytrain ~= 3) = -1;
+            Ytrain3(Ytrain == 3) = 1;
+            %Modelo3=entrenarSVM(Xtrain,Ytrain3,'classification',boxConstraint,gamma,'lin_kernel');
+            Modelo3=entrenarSVM(Xtrain,Ytrain3,'classification',boxConstraint,gamma); 
+
+            [~,Yest1]=testSVM(Modelo1,Xtest);
+            [~,Yest2]=testSVM(Modelo2,Xtest);
+            [~,Yest3]=testSVM(Modelo3,Xtest);
+            [~,Yest] = max([Yest1,Yest2,Yest3],[],2); 
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            MatrizConfusion=zeros(NumClases,NumClases);
+            for i=1:size(Xtest,1)
+                MatrizConfusion(Yest(i+1),Ytest(i+1))=MatrizConfusion(Yest(i+1),Ytest(i+1)) + 1;
+            end
+            EficienciaTest(fold)=sum(diag(MatrizConfusion))/sum(sum(MatrizConfusion));
+
+        end
+
+        Eficiencia = mean(EficienciaTest);
+        IC = std(EficienciaTest);
+        Texto=['La eficiencia obtenida fue = ', num2str(Eficiencia),' +- ',num2str(IC)];
+        disp(Texto);
+
+        %%% Fin punto de clasificaci???n %%%
+    end
+
 end
+
